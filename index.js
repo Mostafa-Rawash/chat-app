bd7cd0a98af2e4c74913c03db96eec926b958549
Mostafa-Rawash <mostafa@rawash.com>
2021-05-23 03:16:48 +0200

    .

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+var bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server)
-let port = 3000
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
+  res.sendFile(__dirname + "/index.html" );
+});
 
 app.post("/users", (req, res) => {
     console.log(req.body.name);
-    res.sendFile(__dirname + "/chat.html")
-})
-
+    res.sendFile(__dirname + "/chat.html");
+});
 
 io.on("connection", (socket) => {
-    console.log("socket is worked");
+  console.log("socket is worked");
 
 
-    socket.on("hi", msg => {
-        console.log(msg);
-        socket.emit("server messaga" , msg)
-    })
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  socket.on("hi", (msg) => {
+    console.log(msg);
+    socket.emit("hello", msg);
+  });
+});
 
 
 server.listen(port, () => {
-    console.log(
-        "server worked in port 3000"
-    );
-})
+  console.log("server worked in port 3000");
+});
+
+
